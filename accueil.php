e7110e8bffa7a7e893f2d7a0e546568f0654bf20
Flo. G <36236404+Tanaele@users.noreply.github.com>
2018-02-07 16:57:29 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -14,20 +14,16 @@
         <h1>Accueil</h1>
         
         <h2>Simpliathlon, un triathlon organisé top chrono !</h2>
+
+    	<form>
+	    	<label for="email">Votre e-mail : </label>
+		<input type="email" name="email" id="email" /><br/>
+		<label for="mdp">Votre mot de passe : </label>
+		<input type="password" name="mdp" id="mdp"/><br/>
+		<input type="button" value="Connexion"/>
+	</form>
 		
-		<form method="post" action="traitement.php">
- 
-			<fieldset id="connexion">
-				<legend>Connexion</legend>
-					<label for="email">Votre e-mail : </label>
-					<input type="email" name="email" id="email" /><br/>
-					<label for="mdp">Votre mot de passe : </label>
-					<input type="password" name="mdp" id="mdp"/><br/>
-					<input type="button" value="Connexion"/>
-			</fieldset>
-		</form>
-		
-		<p><a href="mdp_oublie.php">Mot de passe oublié ?</a></p>
+	<p><a href="mdp_oublie.php">Mot de passe oublié ?</a></p>
     </div>
     
     <!-- Pied de page -->
